9139ed7989655c04067c1a491a47e464f7e0948f
halimAhtasham <ahtashamhalim@gmail.com>
2025-02-05 00:17:53 +0600

    Update comments in Queue class for clarity and localization

--- Queue.java
+++ Queue.java
@@ -12,15 +12,15 @@ public class Queue {
             this.queue = new int[n];
         }
     
-        // -1 means array has no element
+        // -1 mane khali array
         public boolean isEmpty(){
             return rear == -1 && front == -1;
         }
     
         // rear diya value dei, so rear jodi (size - 1) hoy, tahole array full hoye geche
-        public boolean isFull(){
-            return rear == size - 1;
-        }
+        // public boolean isFull(){
+        //     return rear == size - 1;
+        // }
     
     
         // start adding with rear
@@ -28,7 +28,7 @@ public class Queue {
             if ((rear + 1) % size == front) {
                 return -1;
             }
-            // queue khali thakle ekta cell add korbo, mane -1 theke 0 index e
+            // queue khali thakle ekta cell add korbo, mane -1 theke 0
             else if (isEmpty()) {
                 rear = front = 0;
             }
