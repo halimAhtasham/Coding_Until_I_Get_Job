ea7147edd932c5a328c9ea96e580aef65abd6c84
halimAhtasham <ahtashamhalim@gmail.com>
2025-02-04 22:26:42 +0600

    Add Queue class implementation with basic operations

---
+++ Queue.java
@@ -0,0 +1,44 @@
+public class Queue {
+    public static int front = -1;
+    public static int rear = -1;
+    public static int size;
+    public static int[] queue;
+
+    // always insert from rear and delete from front
+
+    public Queue(int n){
+        this.size = n;
+        int[] queue = new int[n];
+    }
+
+    // -1 means array has no element
+    public boolean isEmpty(){
+        return rear == -1 && front == -1;
+    }
+
+    // rear diya value dei, so rear jodi (size - 1) hoy, tahole array full hoye geche
+    public boolean isFull(){
+        return rear == size - 1;
+    }
+
+    public int add(int data){
+        if (isFull()) {
+            System.out.println("Queue is Full");
+            return -1;
+        }
+
+        // queue khali thakle ekta cell add korbo, mane -1 theke 0 index e
+        else if (isEmpty()) {
+            rear = front = 0;
+        }
+        rear++;
+        return queue[rear] = data;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
